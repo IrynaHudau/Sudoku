d7be6d8ba36df8889a5e435c087155183af0a0ea
Iryna Hudava <pir0zh0k@Irynas-MacBook-Pro.local>
2021-03-14 14:25:31 -0400

    removed unused import

--- src/components/AppBar/ToolBar/ToolBar.js
+++ src/components/AppBar/ToolBar/ToolBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import cssStyles from '../ToolBar/ToolBar.module.css';
 import MenuIcon from '../MenuIcon/MenuIcon';
-import NavigationItems from '../Navigation/NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={cssStyles.toolbar}>  
